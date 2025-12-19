424c45ff09fdd495a2504a73f1f21a719353614d
Joseph Forbes <josephforbes717@gmail.com>
2025-12-18 19:22:17 -0700

    Initial File Upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let globalClicks = 0;
+let localClicks = {
+  confirmed: 0,
+  pending: 0
+}
+let confirmedClicks = localClicks.confirmed;
+let pendingClicks = localClicks.pending;
+
+
+let button = document.getElementById("clicker-button");
+let clickCount = document.getElementById("click-count");
+
+const backendUrl = 'http://clicker-backend-production-becd.up.railway.app'; 
+
+button.onclick = async function () {
+  pendingClicks++;
+  render();
+
+  try {
+    const res = await fetch(`${backendUrl}/click`, { method: 'POST' });
+    const data = await res.json();
+
+    // Server confirmed one click
+    confirmedClicks = data.global_clicks;
+    pendingClicks--;
+    render();
+  } catch (err) {
+    console.error('Failed to send click to backend:', err);
+
+    // rollback failed click
+    pendingClicks--;
+    render();
+  }
+};
+
+function render() {
+  clickCount.textContent = confirmedClicks + pendingClicks;
+}
+
+async function updateTotal() {
+  try {
+    const res = await fetch(`${backendUrl}/total`);
+    if (!res.ok) throw new Error('Network response was not ok');
+
+    const data = await res.json();
+
+    // Only update confirmed count
+    confirmedClicks = data.global_clicks;
+    render();
+  } catch (err) {
+    console.error('Failed to fetch total:', err);
+  }
+}
+
+updateTotal(); // initialize global total on page load
+setInterval(updateTotal, 10000); // update every 5 seconds
